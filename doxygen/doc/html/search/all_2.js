66e7a08ab270d2d037e6d39976a443f406f2b204
Ugo Pattacini <ugo.pattacini@iit.it>
2014-07-11 18:15:00 +0200

    added doxygen documentation

---
+++ doxygen/doc/html/search/all_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['readme',['README',['../md_C:_dev_icub-contrib-iit_himrep_modules_linearClassifierModule_README.html',1,'']]]
+];
---
+++ doxygen/doc/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "clrs",
+  1: "cls",
+  2: "r"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "groups",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Modules",
+  2: "Pages"
+};
+
